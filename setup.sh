c0895738f818fc958a79f986583d05f965e60ed3
Chrxstxqn <137430908+Chrxstxqn@users.noreply.github.com>
2023-08-26 10:11:00 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "                Installing \033[91mRed\033[0mDDoS Tool,"
+echo -e "                Installing \033[91mFSK\033[0mDDoS Tool,"
 echo -e "                Please Wait..."
 apt-get -y install python-pip > /dev/null 2>&1
 apt-get -y install python3-pip > /dev/null 2>&1
@@ -9,5 +9,5 @@ pip install tqdm > /dev/null 2>&1
 pip install pyfiglet==0.7 > /dev/null 2>&1
 pip install pyarmor > /dev/null 2>&1
 clear
-echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mDDoS Tool was installed successfully."
-echo -e "\n\nTo run it type '\033[1;4mpython3 dist/fsk-dos.py \033[0m'\n\n"
+echo -e "\033[92m[*]\033[0m \033[91mRed\033[0mFSK DDoS Tool was installed successfully."
+echo -e "\n\nTo run it type '\033[1;4mpython3 kozak/fsk-dos.py \033[0m'\n\n"
